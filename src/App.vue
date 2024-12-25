96e0463be4c0325327913cf2de94a82eab027489
zzyi <uniminio@qq.com>
2024-12-25 15:21:51 +0800

    chore: update Tauri dependencies and enhance window configuration
    - Updated "@tauri-apps/api" to version 2.1.1 in package.json and pnpm-lock.yaml.
    - Added "@tauri-apps/plugin-process" as a new dependency in package.json and pnpm-lock.yaml.
    - Modified App.vue to include a draggable region and adjusted background color.
    - Integrated createTray function in main.ts for tray icon functionality.
    - Updated Cargo.toml and Cargo.lock to include "tauri-plugin-process".
    - Enhanced tauri.conf.json to adjust window dimensions and enable transparency.
    - Expanded permissions in default.json for additional window controls.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex items-center justify-center min-h-screen bg-gray-100">
-    <div class="w-full max-w-md p-4">
+  <div
+    data-tauri-drag-region
+    class="flex items-center justify-center min-h-screen"
+    style="background-color: rgba(255, 255, 255, 0.9)"
+  >
+    <div data-tauri-drag-region class="w-full max-w-md p-4">
       <textarea
         class="w-full p-3 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent resize-none"
         rows="6"
---
+++ src/tray.ts
@@ -0,0 +1,56 @@
+import { TrayIcon } from "@tauri-apps/api/tray";
+import { Menu } from "@tauri-apps/api/menu";
+import { getCurrentWindow } from "@tauri-apps/api/window";
+import { exit } from "@tauri-apps/plugin-process";
+
+export default async function tray_init() {
+  const menu = await Menu.new({
+    items: [
+      {
+        id: "quit",
+        text: "退出",
+        action: async () => {
+          // 退出逻辑
+          await exit();
+        },
+      },
+    ],
+  });
+
+  const options = {
+    icon: "icons/128x128.png",
+    menu,
+    menuOnLeftClick: false,
+    // 托盘行为
+    action: (event: any) => {
+      if (
+        event.type === "Click" &&
+        event.button === "Left" &&
+        event.buttonState === "Down"
+      ) {
+        console.log("单击事件");
+        // 显示窗口
+        winShowFocus();
+      }
+    },
+  };
+
+  async function winShowFocus() {
+    // 获取窗体实例
+    const win = getCurrentWindow();
+    // 检查窗口是否见，如果不可见则显示出来
+    if (!(await win.isVisible())) {
+      win.show();
+    } else {
+      // 检查是否处于最小化状态，如果处于最小化状态则解除最小化
+      if (await win.isMinimized()) {
+        await win.unminimize();
+      }
+      // 窗口置顶
+      await win.setFocus();
+    }
+  }
+
+  const tray = await TrayIcon.new(options);
+  return tray;
+}
